f7f477d7c1dfbfbc7281faddd75a61f97bf92bf7
Fidel Choque <devcrash03@gmail.com>
2024-11-19 20:13:54 +0100

    feat: add class

--- src/pages/Letters.tsx
+++ src/pages/Letters.tsx
@@ -8,13 +8,24 @@ export const Letters = () => {
     <>
     <div className="container mx-auto bg-indigo-300 size-11/12 ">
       <h1 className="text-2xl text-center p-2">Christmas Letters</h1>
-        <ul>
-          {
-          cartas?.map( carta => (
-            <li key={carta.id}>{carta.titleCard}</li>
-          ))
-          }
-        </ul>
+      <table className="table-auto w-full mt-2">
+        <thead className="bg-gray-300">
+          <tr className="border-b border-orange-300 uppercase">
+            <th >Title Card</th>
+            <th >Content</th>
+            <th>Read date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cartas?.slice(0,20).map( carta => (
+            <tr className=" hover:bg-orange-200" key={carta.id} >
+              <td className="px-4 py-1">{carta.titleCard}</td>
+              <td className="px-4 py-1">{carta.content}</td>
+              <td className="px-4 py-1">{carta.date_read}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
         
     </div>
     </>
