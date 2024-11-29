5ed5028c9d453e97bb954f08c4eb4ae5936b59ce
Fidel Choque <devcrash03@gmail.com>
2024-11-29 08:35:16 +0100

    feat: change preview card

--- src/pages/Letters.tsx
+++ src/pages/Letters.tsx
@@ -8,31 +8,18 @@ export const Letters = () => {
     <>
     <div className="h-[44rem] grid bg-slate-400 overflow-scroll">
       <h1 className="text-2xl text-center p-2">Christmas Letters</h1>
-      <table className="table-auto w-full mt-2 text-sm text-left">
-        <thead className="bg-gray-300 font-medium">
-          <tr className="border-b border-orange-300">
-            <th className="py-2 pr-3">Title Card</th>
-            <th className="py-2 pr-3">Content</th>
-            <th className="py-2 pr-3">Was Read</th>
-          </tr>
-        </thead>
-        <tbody className="divide-y">
-          {cartas?.slice(0,20).map( carta => (
-            <tr className=" hover:bg-orange-200" key={carta.id} >
-              <td className="px-1 py-2 whitespace-nowrap">{carta.titleCard}</td>
-              <td className="px-1 py-2 whitespace-nowrap">{carta.content}</td>
-              <td className="px-1 py-2 whitespace-nowrap">
-                <span className={`px-3 py-2 rounded-full font-semibold text-xs ${carta.wasRead ? "text-green-600" : "text-blue-600 bg-blue-50"}`}>
-                  {carta.wasRead}
-                </span>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-        
+      <div className="w-full min-h-[39rem] grid gap-2 grid-cols-3 place-content-between bg-red-300 md:max-w-full md:grid-cols-5 ms:grid-cols-2">
+        {cartas?.slice(0,20).map( carta => (
+          <div className='max-w-md mx-auto bg-gradient-to-r from-cyan-100 from-10% via-sky-100 via-30% to-red-100 to to-90% rounded-xl shadow-md md:max-w-2xl md:flex'> {/*se le quito el */}
+            <div key={carta.id} className='p-4'>
+              <div  className='tracking-wide text-center text-sm text-indigo-500 font-semibold'>{carta.titleCard}</div>
+              <p className='mt-1 text-slate-500'>{carta.date_read}</p>
+              <button className='bg-indigo-500 hover:bg-indigo-700 text-white py-1 px-2 rounded'>Abrir carta</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
     </>
-    
   )
 }
