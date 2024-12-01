0b4ed9c171f49dc42347ec7f78c07b711675c941
Fidel Choque <82333258+fidelChoqueDev@users.noreply.github.com>
2024-12-01 19:05:44 +0100

    feat:change URL cards

--- src/utils/getCardsData.ts
+++ src/utils/getCardsData.ts
@@ -1,7 +1,7 @@
 import {Card} from "../interface/index";
 
-const cardsURL = "https://673b2441339a4ce4451ad0d8.mockapi.io/cards"
-/*https://vivacious-elegance-production.up.railway.app/api/v1/christmas-cards*/
+const cardsURL = "https://vivacious-elegance-production.up.railway.app/api/v1/christmas-cards"
+/* https://673b2441339a4ce4451ad0d8.mockapi.io/cards*/
 
 export const getCardsData = async () => {
 	const data = await fetch(cardsURL)
