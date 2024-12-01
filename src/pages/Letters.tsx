8d9492d00292e60db5165dc5663d023207146e2f
Fidel Choque <82333258+fidelChoqueDev@users.noreply.github.com>
2024-12-01 18:57:35 +0100

    feat: Add component modal

--- src/pages/Letters.tsx
+++ src/pages/Letters.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useFetch } from "../hooks/useFetch"
+import Modal from "../utils/Modal";
 
 
 export const Letters = () => {
@@ -8,9 +9,9 @@ export const Letters = () => {
   
   return (
     <>
-    <div className="h-[44rem] grid bg-slate-400 overflow-scroll">
-      <h1 className="text-2xl text-center p-2">Christmas Letters</h1>
-      <div className="w-full min-h-[39rem] grid gap-2 grid-cols-3 place-content-between bg-red-300 md:max-w-full md:grid-cols-5 ms:grid-cols-2">
+    <div className="h-full flex flex-col bg-transparent overflow-scroll">
+      <h1 className="text-2xl text-center p-1">Christmas Letters</h1>
+      <div className="w-full grid gap-2 grid-cols-3 place-content-between bg-transparent md:max-w-full md:grid-cols-5 ms:grid-cols-2">
         {cartas?.slice(0,20).map( carta => (
           <div className='max-w-md mx-auto bg-gradient-to-r from-cyan-100 from-10% via-sky-100 via-30% to-red-100 to to-90% rounded-xl shadow-md md:max-w-2xl md:flex'> {/*se le quito el */}
             <div key={carta.id} className='p-4'>
@@ -25,7 +26,15 @@ export const Letters = () => {
                 <div className="flex flex-col gap-4">
                   <h2 className="text-2xl">{carta.titleCard}</h2>
                   <p>{carta.content}</p>
-                  <hr className="border-t-solid border-1 border-gray-50"/>
+                  <hr className="border-t-solid border-1 border-gray"/>
+                  <div className="flex flex-row justify-center">
+                    <button 
+                      className="border border-neutral-300 rounded-lg py-1.5 px-10 bg-blue-500 hover:bg-blue-600 text-white"
+                      onClick={()=>setOpen(false)}
+                    >
+                      Close
+                    </button>
+                  </div>
                 </div>
               </Modal>
             </div>
