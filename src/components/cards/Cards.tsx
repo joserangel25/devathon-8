c8d27ad6d8f494b889fcfb467f045edc3426d4a6
Fidel Choque <82333258+fidelChoqueDev@users.noreply.github.com>
2024-12-07 20:02:56 +0100

    feat: change bg

--- src/components/cards/Cards.tsx
+++ src/components/cards/Cards.tsx
@@ -12,14 +12,14 @@ interface CardProps{
 
 export const Cards: React.FC<CardProps> = ({id, titleCard, content, date_read, wasRead, onOpenModal}) => {
   return (
-    <div className='bg-white shadow-md rounded-lg p-4'>
+    <div className='bg-green-500 shadow-md rounded-lg p-4'>
         <h2 className='text-lg font-semibold'>{titleCard}</h2>
         <p className='text-gray-600'>{date_read}</p>
         <div>
           <span
             className={`inline-block w-4 h-4 mr-2 rounded-full ${wasRead ? "bg-green-500":"bg-green-300"}`}
           ></span>
-          <span>{wasRead : "Leido":"Sin leer"}</span>
+          <span>{wasRead? "Leido":"Sin leer"}</span>
         </div>
         <button 
         	className='mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600'
